1840eb0187e782bc019173909617d5226809219e
v119847_ual <riddhivardhan.dubey@united.com>
2024-02-14 09:54:33 +0530

    Add project files.

---
+++ FirstXamarinProject/FirstXamarinProject/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FirstXamarinProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+
+        {
+            InitializeComponent();
+           
+          
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (username.Text=="admin")
+            {
+                if (password.Text == "admin")
+                {
+                    DisplayAlert("Success", "Login success", "ok");
+                }
+                else
+                {
+                    DisplayAlert("Alert", "Password is wrong", "Ok");
+                }
+            }
+            else
+            {
+                DisplayAlert("Alert", "Username is wrong", "Ok");
+                username.Text = "";
+                password.Text = "";
+            }
+        }
+
+    }
+    }
+
