5fc5a14e28714ee10ea06bd49f88602e1edd9a94
v119847_ual <riddhivardhan.dubey@united.com>
2024-02-16 00:25:06 +0530

    layout design assignment

--- FirstXamarinProject/FirstXamarinProject/App.xaml.cs
+++ FirstXamarinProject/FirstXamarinProject/App.xaml.cs
@@ -10,7 +10,7 @@ namespace FirstXamarinProject
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new Page2();
         }
 
         protected override void OnStart()
--- FirstXamarinProject/FirstXamarinProject/MainPage.xaml.cs
+++ FirstXamarinProject/FirstXamarinProject/MainPage.xaml.cs
@@ -17,7 +17,7 @@ namespace FirstXamarinProject
         {
             InitializeComponent();
 
-        
+            
 
 
         }
